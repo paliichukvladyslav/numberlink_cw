1f8ce30a26470261fedddd38e4cd690c2da423f8
Vladyslav Paliichuk <palijczukvladyslav@gmail.com>
2025-05-21 18:06:48 +0000

    [feat] Improve UI support

--- task34/include/ui.h
+++ task34/include/ui.h
@@ -5,12 +5,21 @@
 #include <iostream>
 #include <string>
 
+#define TEMP_FNAME "/tmp/numberlink.txt"
+
 class TextUI {
+private:
+  bool file_exists(std::string &filename);
 public:
+
   void show_main_menu();
-  int ask_choice();
+  void show_secondary_menu();
+
+  int ask_choice(int min, int max);
   int ask_gridsize();
   std::string ask_filename();
+  bool ask_editor();
+
   void print(std::string &msg);
 };
 
--- task34/ui.cc
+++ task34/ui.cc
@@ -1,6 +1,8 @@
 /* ui.cc */
 #include <iostream>
 #include <limits>
+#include <fstream>
+#include <cstdlib>
 #include "ui.h"
 
 void TextUI::show_main_menu() {
@@ -11,11 +13,24 @@ void TextUI::show_main_menu() {
   std::cout << "Ваш вибір: ";
 }
 
-int TextUI::ask_choice() {
-  int choice;
-  std::cin >> choice;
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  return choice;
+void show_secondary_menu() {
+  std::cout << "\n=== Numberlink ===\n";
+  std::cout << "1. Повернутись до головного меню\n";
+  std::cout << "2. Вийти\n";
+  std::cout << "Ваш вибір: ";
+}
+
+int TextUI::ask_choice(int min, int max) {
+  while(true) {
+    int choice;
+    std::cin >> choice;
+    if (std::cin.fail() || choice < min || choice > max) {
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Вибір виходить за межі\n";
+      continue;
+    }
+    return choice;
+  }
 }
 
 int TextUI::ask_gridsize()  {
@@ -26,11 +41,31 @@ int TextUI::ask_gridsize()  {
   return n;
 }
 
+bool TextUI::file_exists(std::string &filename) {
+  std::ifstream f(filename.c_str());
+  return f.good();
+}
+
 std::string TextUI::ask_filename() {
   std::cout << "Введіть назву файлу: ";
   std::string fname;
   std::getline(std::cin, fname);
-  return fname;
+  if (file_exists(fname)) {
+    return fname;
+  } else {
+    std::cerr << "Файл не існує";
+    throw "invalid file";
+  }
+}
+
+bool TextUI::ask_editor() {
+  char *editor = std::getenv("EDITOR");
+  if (!editor) editor = (char *)"nano";
+
+  std::string command = std::string(editor) + " " + TEMP_FNAME;
+  int status = std::system(command.c_str());
+
+  return status == 0;
 }
 
 void TextUI::print(std::string &msg) {
